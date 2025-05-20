3d2470a2fa443a649888d7597017108f0690b0b0
LEEHAN <downfallice@gmail.com>
2025-05-20 23:25:15 +0800

    0520exercise

--- other_nowcoder_exe/hj16_购物单.py
+++ other_nowcoder_exe/hj16_购物单.py
@@ -1,5 +1,4 @@
-n, m = map(int, input().split())
-# n, m =
+n, m = map(int, input().split()) # n为预算, m为物品总数
 goods = [] # 存储商品信息的二维列表
 for i in range(m):
     s = input()
@@ -8,3 +7,15 @@ for i in range(m):
     tempw = int(w)
     tempq = int(q)
     goods.append([tempv, tempw, tempq])
+satis = [] # 存储商品的价格与满意度
+for i in range(m):
+    if goods[i][2] == 0: # 该商品是主件
+        rec = i # 记录主件的下标
+        tempv = goods[i][0]
+        temps = goods[i][0] * goods[i][1] # 该组合商品的满意度
+        satis.append([tempv, temps])
+    else: # 该商品是附件
+        recf = goods[i][2] - 1 # 记录该附件的主件的下标
+        tempv = goods[i][0] + goods[recf][0] # 该组合商品的价格
+        temps = goods[i][0] * goods[i][1] + goods[recf][0] * goods[recf][1]
+        satis.append([tempv, temps])
