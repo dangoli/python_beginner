5e5ded6948e2ff4fcb1f7239a7221b81cc57dd83
LEEHAN <downfallice@gmail.com>
2025-06-02 23:52:31 +0800

    Create hj20_密码验证合格程序.py

---
+++ other_nowcoder_exe/hj20_密码验证合格程序.py
@@ -0,0 +1,11 @@
+import sys
+lines = [] # 空列表接收输入的密码
+while True:
+    line = input()
+    if line == "": #如果是空行就终止
+        break
+    lines.append(line)
+
+def verify(pw): # 定义验证函数
+    
+print(lines)
