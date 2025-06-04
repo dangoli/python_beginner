deaf3b0b4a48719137500f253dc67ea76d096c52
LEEHAN <downfallice@gmail.com>
2025-06-04 23:58:36 +0800

    Update hj20_密码验证合格程序.py

--- other_nowcoder_exe/hj20_密码验证合格程序.py
+++ other_nowcoder_exe/hj20_密码验证合格程序.py
@@ -6,7 +6,14 @@ while True:
         break
     lines.append(line)
 
+def character(pw): # 定义字符验证函数
+    count = 0 #初始化字符种类
+    for i in pw:
+        ascii_code = ord(i) # 获取字符的ASCII码
+        if 48 <= ascii_code <= 57: # 数字
+            count += 1 #包含数字类字符
+            
+
 def verify(pw): # 定义验证函数
     if len(pw) < 8: # 长度小于8就不合格
         print("NG")
-print(lines)
